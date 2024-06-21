3b5bcb05ad78256b7b22e1b7f4dcab62e6fd41b3
Vaibhav Ranga <vaibhavranga2@gmail.com>
2024-06-21 22:42:39 +0530

    Added bundle decode extension

---
+++ BundleDecodeExtension.playground/Contents.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+extension Bundle {
+    func decode<T: Codable>(_ file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        
+        do {
+            return try decoder.decode(T.self, from: data)
+        } catch DecodingError.keyNotFound(let key, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' - \(context.debugDescription)")
+        } catch DecodingError.typeMismatch(_, let context) {
+            fatalError("Failed to decode \(file) from bundle due to type mismatch - \(context.debugDescription)")
+        } catch DecodingError.valueNotFound(let type, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing \(type) value - \(context.debugDescription)")
+        } catch DecodingError.dataCorrupted(_) {
+            fatalError("Failed to decode \(file) from bundle because it appears to be invalid JSON.")
+        } catch {
+            fatalError("Failed to decode \(file) from bundle: \(error.localizedDescription)")
+        }
+    }
+}
---
+++ Comparable.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+struct User: Identifiable, Comparable {
+    let id = UUID()
+    var firstName: String
+    var lastName: String
+
+    static func <(lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+}
+
+let users = [
+    User(firstName: "Arnold", lastName: "Rimmer"),
+    User(firstName: "Kristine", lastName: "Kochanski"),
+    User(firstName: "David", lastName: "Lister"),
+].sorted()
+
+print(users)
