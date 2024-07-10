3bbd7eda5c97bc2189a7a1b6c18fe8690d7cbd7e
Vaibhav Ranga <vaibhavranga2@gmail.com>
2024-07-11 03:36:07 +0530

    added computed properties and property observers

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -15,3 +15,50 @@ let names1 = ["Dinesh", "Karuna"]
 
 //names1.append("Vaibhav")      //gives an error as names1 was created by let
 
+
+
+
+
+
+print("--------------------------")
+
+//computed properties
+struct Person {
+    var age: Int
+
+    var ageInDogYears: Int {
+        get {
+            return age * 7
+        }
+    }
+}
+
+var fan = Person(age: 5)
+print(fan.ageInDogYears)
+
+
+
+
+
+print("--------------------------")
+
+//property observers
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
